1a2253c27da30299eeb93ddc9ff2c5a2c6587007
aswathi <aswathiram01@gmail.com>
2025-10-08 16:46:00 +0530

    Add files via upload

---
+++ App1.jsx
@@ -0,0 +1,36 @@
+
+export default function App1(){
+  return(
+    <div>
+
+        <div class="text-6xl text-center font-bold text-green "><p>Educrib Discover: An All-in-One<br></br>Career Discovery Platform</p></div>
+        <p class="text-center mt-10 text-gray-600 text-2xl">Discover is the admin-side interface for educational institutions to build
+         <br></br>and manage their profiles on Educrib.</p>
+         <div class="flex flex-row ml-40">
+          <div class="ml-80 mt-6 wl-56 bg-gradient-to-tr from-purple-500 to-violet-800 size-32 w-72 h-12 text-center rounded-full pl-6 pt-2 text-white text-2xl"><h3>Visit Discover platform</h3>
+          </div>
+          <div class="mt-10 text-sm text-gray-600">1000+ Institutions onboarded</div>
+         </div>
+        <div class="flex flex-row">
+          <div class=" mt-10 ml-8 bg-gradient-to-tl from-pink-400 to-purple-400 rounded-md size-80 w-[900px] h-[700px]">
+            <p class="text-center text-4xl  text-white pt-10 "><span class="font-bold">Boost converstions,engagement<br></br>and tell better stories<br></br></span><span class="text-xl font-no-bold">Without getting pixelated</span></p>
+            <div class="border border-8 border-black ml-16  w-[750px] h-[450px] rounded-xl mt-10"><img class="w-[750px] h-[435px] " src="/src/assets/images/educrib2.png"></img></div>
+            </div>
+          <div>
+          <div class=" mt-10 ml-8 bg-gradient-to-tl from-violet-400 to-sky-300 rounded-md size-80 w-[400px] h-[250px]">
+            <h1 class="text-center text-blue-100">Get Experience</h1>
+            <div class="border-2 rounded-xl border-black w-[350px] h-[200px] pt-10 ml-6 overflow-hidden "><img class="-mt-10" src="/src/assets/images/educrib2.png"></img></div>
+            <div class=" mt-12  bg-gradient-to-tl from-pink-400 to-orange-300 rounded-md size-80 w-[400px] h-[425px]">
+            <h1 class="text-center text-blue-100">Get Experience</h1>
+            <div class="border-2 rounded-xl border-black w-[300px] h-[380px] pt-10 ml-14 overflow-hidden "><img class="-mt-10 w-[300px] h-[400px]" src="/src/assets/images/educrib3.jpeg"></img></div>
+          </div>
+          </div>
+            </div>
+          <div>
+
+          </div>
+        </div>
+        
+        </div>
+  )
+}
